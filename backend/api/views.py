62815884ba3601df8f8af60beaac645c627be1d0
Sanskar <alreadyselfish@gmail.com>
2023-07-18 03:25:08 +0530

    Added the way to lookup a single Product through pk.

--- backend/api/views.py
+++ backend/api/views.py
@@ -24,23 +24,30 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from api.serializer import ProductSerializer 
 
-@api_view(['PUT', 'DELETE', 'PATCH'])
+@api_view(['PUT', 'DELETE', 'PATCH', 'GET'])
 def product_changes(request, pk):
     try:
         instance = Product.objects.get(pk=pk)
     except Product.DoesNotExist:
-        return Response({"message": "invalid primary key"}, status=400)
+        return Response({"detail": "invalid primary key"}, status=400)
+    
     if request.method == "PUT" or request.method=="PATCH":
         part_bool = (request.method == 'PATCH')
         serializer = ProductSerializer(instance, data=request.data, partial=part_bool)
         if not serializer.is_valid():
-            return Response({"message": "invalid data"}, status=400)
+            return Response({"detail": "invalid data"}, status=400)
         serializer.save()
         return Response(serializer.data, status=201)
 
     if request.method == "DELETE":
         instance.delete()
-        return Response(status=204)
+        message = {}
+        message['detail'] = f"Your instance with primary key {pk} was deleted and no longer exists."
+        return Response(message)
+    
+    if request.method == "GET":
+        serializer = ProductSerializer(instance)
+        return Response(serializer.data, status=200)
 
 
         
